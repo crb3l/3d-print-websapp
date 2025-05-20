9f58bc6aaa9957756e656e1a86f42140a59ace46
unknown <iotheodor@gmail.com>
2025-05-21 01:38:44 +0300

    yet OTHER CHANGES FOR DEPLOYMENT IM GOING MAD

--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -30,7 +30,7 @@ const Signup = () => {
             return;
         }
 
-        const { data, error: signUpError } = await supabase.auth.signUp({
+        const { /*data,*/ error: signUpError } = await supabase.auth.signUp({
             email,
             password,
             options: {
