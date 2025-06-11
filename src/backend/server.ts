d5ab503704d917c592c94f869e9f506b1848ad3d
unknown <iotheodor@gmail.com>
2025-06-11 15:48:52 +0300

    made a separate backend folder to repo it separately

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,16 +1,16 @@
 
-import express, { Request, Response } from 'express';
+import express/*, { Request, Response }*/ from 'express'; // uncomm when uploading to a dB
 import dotenv from 'dotenv';
 import cors from 'cors';
-import multer from 'multer';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import submitOrderRoute from './src/api/submitOrder';
-import resendConfirmationRoute from './src/api/resendConfirmation'
+// import multer from 'multer';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+import submitOrderRoute from './api/submitOrder';
+import resendConfirmationRoute from './api/resendConfirmation'
 
 // Define __filename and __dirname for ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url); // for uploading stuff, but i am not doing that yet
+// const __dirname = path.dirname(__filename); // for uploading stuff, but i am not doing that yet
 
 // // Load environment variables
 dotenv.config();
@@ -18,22 +18,22 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3001;
 
-const storage = multer.memoryStorage();
+// const storage = multer.memoryStorage(); //-- for uploading stuff, but i am not doing that yet
 
-const upload = multer({
-  storage: storage,
-  limits: { fileSize: 50 * 1024 * 1024 }, // 50MB
-});
+// const upload = multer({
+//   storage: storage,
+//   limits: { fileSize: 50 * 1024 * 1024 }, // 50MB
+// });
 
 
 // Middleware
 app.use(express.json());
 app.use(cors());
 
-// Define interface for request with file
-interface MulterRequest extends Request {
-  file?: Express.Multer.File;
-}
+// Define interface for request with file -- for uploading, not yet
+// interface MulterRequest extends Request {
+//   file?: Express.Multer.File;
+// }
 
 // Process 3D print orders
 app.use('/api', submitOrderRoute);
